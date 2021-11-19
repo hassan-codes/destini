b122cee6e5c0bec891084a6ae2f11608aec842dc
Baako Alhassan Mohammed <baakomohammed95@gmail.com>
2021-11-19 03:03:47 +0000

    Create story progress logic

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,6 +1,7 @@
 import 'story.dart';
 
 class StoryBrain {
+  int storyNumber = 0;
   final List<Story> _storyData = [
     Story(
         storyTitle:
@@ -44,14 +45,46 @@ class StoryBrain {
   String getChoice2() {
     return _storyData[0].choice2;
   }
+
+  void nextStory(int choiceNumber) {
+    if (storyNumber == 0) {
+      if (choiceNumber == 1) {
+        storyNumber = 2;
+      } else if (choiceNumber == 2) {
+        storyNumber = 1;
+      }
+    }
+
+    if (storyNumber == 1) {
+      if (choiceNumber == 1) {
+        storyNumber = 2;
+      } else if (choiceNumber == 2) {
+        storyNumber = 3;
+      }
+    }
+
+    if (storyNumber == 2) {
+      if (choiceNumber == 1) {
+        storyNumber = 5;
+      } else if (choiceNumber == 2) {
+        storyNumber = 4;
+      }
+    }
+
+    if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+      restart();
+    }
+  }
+
+  void restart() {
+    storyNumber = 0;
+  }
 }
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
 
 //TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
 
